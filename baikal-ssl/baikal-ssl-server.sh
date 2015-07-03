a7ce1b629fee7a2e9b490bf39ba609c7b6fde6ec
Cyrill Kulka <cyrill.kulka@gmail.com>
2015-07-03 20:00:28 +0200

    fixed initial startup where ENABLE_INSTALL is removed by Baikal during the first HTTP request

--- baikal-ssl/baikal-ssl-server.sh
+++ baikal-ssl/baikal-ssl-server.sh
@@ -18,7 +18,7 @@ IMAGE="ckulka/baikal-ssl:latest"
 NAME="baikal-ssl-server"
 DATA_NAME="$NAME-data"
 
-# Add your exposed GoCD Server ports here
+# Add your exposed ports here
 PORTS="-p 80:80 -p 443:443"
 
 # Add your host mounts here if you want to - for whatever reason - mount directories from your host system instead of the data volume container.
@@ -34,12 +34,24 @@ start() {
 		echo "docker create --name $DATA_NAME $VOLUMES $IMAGE /bin/true"
 		echo -n "Creating new data volume container $DATA_NAME: "
 		docker create --name $DATA_NAME $VOLUMES $IMAGE /bin/true
+                NEW_CONTAINER="TRUE"
 	else
 		echo "Using existing data volume container: $DATA_NAME"
 	fi
 
 	echo "Starting new $IMAGE container: $NAME"
 	docker run -d  $PORTS --volumes-from $DATA_NAME --name $NAME $IMAGE
+
+	# Baikal deletes the initial ENABLE_INSTALL on the first request, we therefore have to recreate it 
+        if [ "x$NEWCONTAINER" = "x" ]; then
+                echo 
+                echo "New Baikal server, preparing ENABLE_INSTALL filemarker"
+                echo "Waiting 10 seconds for apache2 to start"
+                sleep 10
+                echo "Preparing ENABLE_INSTALL filemarker"
+                docker exec $NAME curl -D /dev/null -o /dev/null http://localhost/admin/install/
+                docker exec $NAME touch /var/www/html/Specific/ENABLE_INSTALL
+        fi
 }
 
 stop() {
