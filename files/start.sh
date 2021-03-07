bf3e7f9c5f0075248f19672c24f5bab56cd1dc74
Philipp Neugebauer <ppn@philippneugebauer.net>
2021-03-07 19:24:23 +0100

    enable email notifications vi msmtprc

--- files/start.sh
+++ files/start.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+sed -i "s/SMTP_URL/${SMTP_URL}/g" /etc/msmtprc
+sed -i "s/SMTP_FROM/${SMTP_FROM}/g" /etc/msmtprc
+sed -i "s/SMTP_USER/${SMTP_USER}/g" /etc/msmtprc
+sed -i "s/SMTP_PASSWORD/${SMTP_PASSWORD}/g" /etc/msmtprc
+
 # Inject ServerName and ServerAlias if specified
 APACHE_CONFIG="/etc/apache2/sites-available/000-default.conf"
 if [ ! -z ${BAIKAL_SERVERNAME+x} ]
