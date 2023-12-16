1d1451c8e63d86c71da75fad9db7607ac91aaff9
Cyrill Kulka <ckulka@users.noreply.github.com>
2023-12-16 13:16:43 +0100

    Skip chown command in startup script by setting BAIKAL_SKIP_CHOWN (#138)
    * Ensure correct file permissions, unless behaviour is explicitly disabled
    
    * Documented skipping of chown commands in README.md
    
    * Corrected paths glob pattern in Github workflows
    
    ---------
    
    Co-authored-by: Cyrill Kulka <ckulka@wlgore.com>

--- files/docker-entrypoint.d/httpd/40-fix-baikal-file-permissions.sh
+++ files/docker-entrypoint.d/httpd/40-fix-baikal-file-permissions.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-chown -R www-data:www-data /var/www/baikal
+# Ensure correct file permissions, unless behaviour is explicitly disabled
+if [ -z ${BAIKAL_SKIP_CHOWN+x} ]
+then
+  chown -R www-data:www-data /var/www/baikal
+fi
