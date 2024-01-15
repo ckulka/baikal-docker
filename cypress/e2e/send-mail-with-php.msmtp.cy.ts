c5e04defbc73b385348169a2bd34b5d70de778d0
Cyrill Kulka <ckulka@wlgore.com>
2024-01-15 13:34:09 +0100

    Switched from mailtrap to mailslurper

---
+++ cypress/e2e/send-mail-with-php.msmtp.cy.ts
@@ -0,0 +1,20 @@
+describe("Send mail with PHP", () => {
+  it("Should send an email with PHP", () => {
+    // Send email through PHP (response of mail-test.php is the email subject)
+    cy.request("localhost/mail-test.php").then((response) => {
+      // Verify that the email arrived
+      cy.request({
+        url: "localhost:8085/mail",
+        qs: {
+          message: response.body,
+        },
+      }).should((response) => {
+        expect(response.body.totalRecords).to.eql(1);
+        let mail = response.body.mailItems[0];
+        expect(mail.fromAddress).to.eql("baikal@example.com");
+        expect(mail.toAddresses).to.eql(["to@example.com"]);
+        expect(mail.body).to.eql("Email sent with PHP mail()\n");
+      });
+    });
+  });
+});
