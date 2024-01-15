14eee2c79ce6d4f1f63ce84b90fb9e79ce161093
Cyrill Kulka <ckulka@users.noreply.github.com>
2024-01-15 22:38:34 +0100

    Switched from mailtrap to mailslurper (#156)

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from "cypress";
 
-export default defineConfig({
+let config = {
   e2e: {
     supportFile: false,
-    excludeSpecPattern: "**/*.mailtrap.cy.ts",
+    screenshotsFolder: "cypress/screenshots",
+    excludeSpecPattern: "",
   },
-});
+  reporterOptions: {
+    mochaFile: "results/my-test-output.txt",
+    toConsole: true,
+  },
+};
+
+if (process.env.CYPRESS_MSMTP_ENABLED ?? false) {
+  config.e2e.screenshotsFolder = "cypress/screenshots/msmtp";
+} else {
+  config.e2e.excludeSpecPattern = "**/*.msmtp.cy.ts";
+}
+
+export default defineConfig(config as Cypress.ConfigOptions);
