725f5ce91a5dc1ad6b55181782ddeaca039a848c
Cyrill Kulka <ckulka@wlgore.com>
2024-05-04 18:09:29 +0530

    Added test to verify PHP version is not exposed

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,12 +6,10 @@ let config = {
     screenshotsFolder: "cypress/screenshots",
     excludeSpecPattern: "",
   },
-  reporterOptions: {
-    mochaFile: "results/my-test-output.txt",
-    toConsole: true,
-  },
 };
 
+// If MSMTP is enabled, we will use a different screenshots folder,
+// otherwise exclude the MSMTP tests.
 if (process.env.CYPRESS_MSMTP_ENABLED ?? false) {
   config.e2e.screenshotsFolder = "cypress/screenshots/msmtp";
 } else {
---
+++ cypress/e2e/expose-php.cy.ts
@@ -0,0 +1,10 @@
+// Test to check if PHP version is hidden in the response headers
+// See https://github.com/ckulka/baikal-docker/issues/111
+describe("Hidden PHP version header (#111)", () => {
+  it("Should not expose PHP version", () => {
+    cy.request("localhost").should((response) => {
+      expect(response.headers["x-powered-by"], "HTTP header 'x-powered-by'").to
+        .be.undefined;
+    });
+  });
+});
