6e2c08fd342589d3f93e9ce057c2d65cf01212d8
Cyrill Kulka <ckulka@wlgore.com>
2025-01-22 19:27:47 +0100

    Updated NPM dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,18 @@
 import tseslint from "typescript-eslint";
-import eslintConfigPrettier from "eslint-config-prettier";
+import eslintConfigPrettier, { rules } from "eslint-config-prettier";
 
-export default [...tseslint.configs.recommended, eslintConfigPrettier];
+export default [
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  // Chai assertion friendly configuration
+  {
+    rules: [
+      {
+        files: ["*.test.js", "*.spec.js"],
+        rules: {
+          "no-unused-expressions": "off",
+        },
+      },
+    ],
+  },
+];
